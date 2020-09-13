1c7c7ef3917d2bc48c031b0a9341160cd4f87bdf
Andreas <a_eder@gmx.net>
2020-09-13 13:07:38 +0200

    test

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path')
 const app = express()
 
 // middleware
+app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // Connect MongoDB
@@ -26,10 +27,6 @@ app.use('/api/items', items)
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'))
-
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    // })
 }
 
 // Connect Server
